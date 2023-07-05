dff3d6cc3c2ce3989d9e4604015ecc199c21791a
Yevhen <egulyayko@gmail.com>
2023-07-05 20:40:53 +0300

    Решили Task01 на проверку квадрата числа

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -5,4 +5,3 @@ Console.Write("Введите целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
  int square = number * number;
  Console.WriteLine($"Квадрат числа {number} равен: {square}");
-
---
+++ Task01/Program.cs
@@ -0,0 +1,17 @@
+﻿// На ввод два числа. Проверить является ли второе число квадратом первого.
+
+
+Console.Write("Введите первое целое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 == number1 * number1)
+
+{
+    Console.WriteLine($"Да, число {number2} является квадратом числа {number1}");
+}
+else
+{
+    Console.WriteLine($"Нет, число {number2} НЕ является квадратом числа {number1}");
+}

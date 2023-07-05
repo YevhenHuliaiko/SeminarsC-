a5cd3e65ab0f8aff60149efadd7fac590c0e1d3e
Yevhen <egulyayko@gmail.com>
2023-07-05 21:20:43 +0300

    Решили Task03 дни недели

---
+++ Task03/Program.cs
@@ -0,0 +1,66 @@
+﻿// Console.Write("Ведите номер дня недели: ");
+// string number = Console.ReadLine();
+
+// if (number == "1")
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// else if (number == "2")
+// {
+//     Console.WriteLine("Вторник");
+// }
+// else if (number == "3")
+// {
+//     Console.WriteLine("Среда");
+// }
+// else if (number == "4")
+// {
+//     Console.WriteLine("Четверг");
+// }
+// else if (number == "5")
+// {
+//     Console.WriteLine("Пятница");
+// }
+// else if (number == "6")
+// {
+//     Console.WriteLine("Суббота");
+// }
+// else if (number == "7")
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+// else 
+// {
+// Console.WriteLine("Ошибка ввода");
+// }
+
+Console.Write("Ведите номер дня недели: ");
+string number = Console.ReadLine();
+
+switch (number)
+{
+    case "1":
+        Console.WriteLine("Понедельник");
+        break;
+    case "2":
+        Console.WriteLine("Ворник");
+        break;
+    case "3":
+        Console.WriteLine("Среда");
+        break;
+    case "4":
+        Console.WriteLine("Четверг");
+        break;
+    case "5":
+        Console.WriteLine("Пятница");
+        break;
+    case "6":
+        Console.WriteLine("Суббота");
+        break;
+    case "7":
+        Console.WriteLine("Воскресенье");
+        break;
+    default:
+        Console.WriteLine("Ошибка ввода");
+        break;
+}

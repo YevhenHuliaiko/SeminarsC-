3e6770606ee348de47fef8d46fab2704882f2a4f
Yevhen <egulyayko@gmail.com>
2023-07-06 12:28:27 +0300

    Решили Task05 Вывод от -N до N

---
+++ Task05/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа которая на вход принимае целое число N, а на выходе выдает все целые числа
+// от -N до N
+
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 0) 
+{
+    Console.WriteLine("Ошибка ввода");
+}
+else
+{
+    int counter = -number;
+    while (counter <= number)
+    {
+        Console.Write(counter + " ");
+        counter = counter + 1;
+
+    }
+}

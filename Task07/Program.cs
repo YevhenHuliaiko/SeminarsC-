6d30f693607dc84972b5f9ea91c15b92bb9701fa
Yevhen <egulyayko@gmail.com>
2023-07-06 14:06:19 +0300

    Решили Task07 последняя цифра трехзначного числа

---
+++ Task07/Program.cs
@@ -0,0 +1,18 @@
+﻿// Принимает трехзначное число на входе и на выходе показывает его последнюю цифру
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    number *= -1;
+}
+
+if (number >= 100 && number <= 999)
+{
+    int lastDigit = number % 10;
+    Console.WriteLine($"Последняя цифра: {lastDigit} ");
+}
+else
+{
+    Console.WriteLine("Ошибка ввода");
+}

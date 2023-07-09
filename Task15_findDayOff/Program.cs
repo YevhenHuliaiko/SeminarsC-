3e3156e59ff81286abd003cfbc29016e2f29f50e
Yevhen <egulyayko@gmail.com>
2023-07-10 00:33:46 +0300

    Домашняя работа Task15 Определение того, является ли день выходным.

---
+++ Task15_findDayOff/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 15: Напишите программу, которая принимает на 
+//            вход цифру, обозначающую день недели, и 
+//            проверяет, является ли этот день выходным.
+//            6 -> да
+//            7 -> да
+//            1 -> нет
+
+
+Console.WriteLine("Введите цифру обозначающую день недели:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int day = DefineDayOff(number);
+
+int DefineDayOff(int num)
+{
+    switch (number)
+{
+    case 1:
+        Console.WriteLine("Этот день не является выходным");
+        break;
+    case 2:
+        Console.WriteLine("Этот день не является выходным");
+        break;
+    case 3:
+        Console.WriteLine("Этот день не является выходным");
+        break;
+    case 4:
+        Console.WriteLine("Этот день не является выходным");
+        break;
+    case 5:
+        Console.WriteLine("Этот день не является выходным");
+        break;
+    case 6:
+        Console.WriteLine("Да, выходной");
+        break;
+    case 7:
+        Console.WriteLine("Да, выходной");
+        break;
+    default:
+        Console.WriteLine("Ошибка ввода");
+        break;
+}
+return number;
+}

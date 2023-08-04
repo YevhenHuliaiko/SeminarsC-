a21e14e5b1d469d3f28abc2eff73a2f2becae5b3
Yevhen <egulyayko@gmail.com>
2023-08-04 14:31:44 +0300

    Решили Task63,65,67,69

---
+++ Task65_RecursionDigitFromMtoN/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+//             выведет все натуральные числа в промежутке от M до N.
+//             M = 1; N = 5 -> "1, 2, 3, 4, 5"
+//             M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+Console.WriteLine("Input number 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Input number 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumberRange(number1, number2);
+
+void NaturalNumberRange(int m, int n)
+{
+    if (m > n)
+    {
+        Console.Write($" {m}");
+        NaturalNumberRange(m - 1, n);
+    }
+    else if (m < n)
+    {
+        Console.Write($" {m}");
+        NaturalNumberRange(m + 1, n);
+    }
+    else Console.WriteLine($" {m}");
+}
+
+if (number1 < 0 || number2 < 0)
+{
+    Console.WriteLine("Incorrect numbers");
+    return;
+}
---
+++ Task67_RecursionSumDigits/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+//             возвращать сумму его цифр.
+//             453 -> 12
+//             45 -> 9
+
+
+
+
+// int FactorialRec(int n)
+//  {
+//      if (n == 1) return 1;
+//      return n * FactorialRec(n - 1);
+//  }
+
+//  Console.WriteLine(FactorialRec(5)); // 3628800
+
+
+Console.WriteLine("Input number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumDigit(num);
+Console.WriteLine(sum);
+
+int SumDigit(int number)
+{
+    if (number == 0) return 0;
+    return number % 10 + SumDigit(number / 10); // 453 -> 45 -> 4 | 4%10 + 45%10 + 453%10 + 0 = 12
+}
+
+
+

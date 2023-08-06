211ed1e0ceeb59acdf4fffabd57f7b8e7dbaae5a
Yevhen <egulyayko@gmail.com>
2023-08-06 12:50:39 +0300

    Домашняя работа Task64,66,68

---
+++ Task66_SumNatElementsFromMtoN/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+//             которая найдёт сумму натуральных элементов в промежутке от M до N.
+//             M = 1; N = 15 -> 120
+//             M = 4; N = 8. -> 30
+
+Console.WriteLine();
+Console.WriteLine();
+Console.Write("Введите натуральное значение M: ");
+int valueM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное значение N: ");
+int valueN = Convert.ToInt32(Console.ReadLine());
+
+int SumElements(int m, int n)
+{
+    if (m == n)
+        return n;
+    else if (m < n)
+    {
+        return m + SumElements(m + 1, n);
+    }
+    else
+    {
+        return n + SumElements(n + 1, m);
+    }
+}
+
+bool CheckNatural(int valueM, int valueN)
+{
+    return valueM > 0 && valueN > 0;
+}
+
+if (!CheckNatural(valueM, valueN))
+{
+    if (valueM < 0 && valueN < 0)
+    {
+        Console.WriteLine("Введенные M и N не натуральные. Невозможно выполнить программу!");
+    }
+    else if (valueN < 0)
+    {
+        Console.WriteLine("Введенное число N не натуральное. Невозможно выполнить программу!");
+    }
+    else
+    { Console.WriteLine("Введенное число M не натуральное. Невозможно выполнить программу!"); }
+    return;
+}
+
+
+int res = SumElements(valueM, valueN);
+Console.Write($"Сумма натуральных элементов от М до N: ");
+Console.WriteLine(res);
+Console.WriteLine();
+
+
---
+++ Task68_AckermanFunction/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана 
+//             с помощью рекурсии. Даны два неотрицательных числа m и n.
+//             m = 2, n = 3 -> A(m,n) = 9
+//             m = 3, n = 2 -> A(m,n) = 29
+
+
+
+Console.WriteLine();
+Console.WriteLine();
+Console.Write("Введите натуральное значение M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное значение N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int AckermanFunction(int m, int n)
+{
+
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+    {
+        if (n == 0)
+        {
+            return AckermanFunction(m - 1, 1);
+        }
+
+        return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+    }
+}
+
+bool CheckRange(int numberM, int numberN)
+{
+    return (numberM > 3 &&  numberN >= 11) || ( numberM >= 4 && numberN > 0);
+  
+}
+
+if (CheckRange(numberM, numberN))
+{
+    Console.WriteLine("Превышение допустимого диапазона, введите другие M и N");
+    return;
+}
+
+int ack = AckermanFunction(numberM, numberN);
+Console.WriteLine(ack);
